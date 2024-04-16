30abc9a01e95cbcb5a60b8cf1daab675e11e04dd
nicorss <nicolasrodriguezsanchezmercier@gmail.com>
2024-04-16 20:41:41 +0200

    Starting with BUS
    Created atributtes, constructor and started the run method.

--- CATL/src/main/java/com/mycompany/catl/Bus.java
+++ CATL/src/main/java/com/mycompany/catl/Bus.java
@@ -4,10 +4,27 @@
  */
 package com.mycompany.catl;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author THINKPAD
  */
-public class Bus {
+public class Bus extends Thread{
+    private String identifier;
+
+    public Bus(String identifier) {
+        this.identifier = identifier;
+    }
     
+    public void run(){
+        try {
+            long sleepTime = (long)(Math.random() * 3000 + 2000);
+            sleep(sleepTime);                                                   //Arrival to downtown
+            
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Bus.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
